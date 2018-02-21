f1ace46856d26b3d6ef0f7bfe0e05b4f72af3780
alxsycc <alxsycc@outlook.com>
2018-02-21 20:57:56 -0200

    first line second line

--- bin.sh
+++ bin.sh
@@ -1,7 +1,8 @@
 git add -A
 #git commit -S -m "$(echo alan\nmorais)"
-git commit -F- -S <<EOF
-goes
-here
-EOF
+#git commit -F- -S <<EOF
+#goes
+#here
+#EOF
+git commit -m $'first line\nsecond line'
 git push
