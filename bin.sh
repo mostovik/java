f3666be8584634954cd76ec6e0f881bcd1db72e5
alxsycc <alxsycc@outlook.com>
2018-02-25 20:04:43 -0200

    adcionando git, written by alxsy mostovik 25/02/2018_08:04:43 PM

--- bin.sh
+++ bin.sh
@@ -1,8 +1,4 @@
 git add -A
-#git commit -S -m "$(echo alan\nmorais)"
-#git commit -F- -S <<EOF
-#goes
-#here
-#EOF
-git commit -m "`seq -s"," 1 100`"
-git push
+git commit -S -m "$*, written by alxsy mostovik `date +%d/%m/%Y_%r`"
+git push -f
+
